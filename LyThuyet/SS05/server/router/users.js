1533a54dd29162a73360e065f55658c913ace4f8
May <22424005@student.hcmus.edu.vn>
2026-04-01 15:38:20 +0700

    update SS05

---
+++ LyThuyet/SS05/server/router/users.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import usersController from '../controllers/users.js';
+
+const router = express.Router();
+
+router.post('/', usersController.createUser);
+router.get('/', usersController.getUsers);
+
+export default router;
